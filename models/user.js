3f6076178d358b5d6a89a8cd0e6615df7453d13c
Eduard <eduard_king_anterola@dlsl.edu.ph>
2025-10-11 18:49:10 +0800

    feat(User): add placeholder user schema

--- models/user.js
+++ models/user.js
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true }
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+export default User;
