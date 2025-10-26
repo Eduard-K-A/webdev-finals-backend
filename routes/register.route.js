4bee53ee99091f0ddc0ba30d41c8c687a21675bc
Eduard <eduard_king_anterola@dlsl.edu.ph>
2025-10-26 17:17:04 +0800

    refactor: change url route capitaliaztion

--- routes/register.route.js
+++ routes/register.route.js
@@ -3,7 +3,7 @@
  */
 
 import express from 'express';
-import { register } from '../controllers/auth/index.js';
+import { register } from '../controllers/auth/index.controller.js';
 import { validateRegister } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
@@ -15,6 +15,6 @@ const router = express.Router();
  * @body    {username, firstName, lastName, email, password}
  * @returns {message, user: {id, username, email}}
  */
-router.post("/register", validateRegister, register);
+router.post("/Register", validateRegister, register);
 
 export default router;
