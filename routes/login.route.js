24e11b56f8cb08e68da440fd5afe611633f45f20
Eduard <eduard_king_anterola@dlsl.edu.ph>
2025-10-28 12:32:06 +0800

    feat: add login route

---
+++ routes/login.route.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { login } from '../controllers/auth/login.controller.js';
+import { validateLogin } from '../middleware/login.validator.js';
+
+const router = express.Router();
+
+/**
+ * @route   POST /auth/login
+ * @desc    Login an existing user
+ * @access  Public
+ * @body    {username, password}
+ * @returns {message, user: {id, username, email, firstName, lastName, roles}}
+ */
+router.post("/login", validateLogin, login);
+
+export default router;
