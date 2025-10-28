aaadb345f6cb49526c1d95dfb7715c42597cb140
Eduard <eduard_king_anterola@dlsl.edu.ph>
2025-10-28 12:36:29 +0800

    chore: rename file

--- routes/register.route.js
+++ routes/register.route.js
@@ -1,7 +1,7 @@
 
 import express from 'express';
 import { register } from '../controllers/auth/register.controller.js';
-import { validateRegister } from '../middleware/auth.validator.js';
+import { validateRegister } from '../middleware/register.validator.js';
 
 const router = express.Router();
 
