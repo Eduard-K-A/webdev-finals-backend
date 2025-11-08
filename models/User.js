d7035552455768976995cf16552c2d2773624614
Eduard <eduard_king_anterola@dlsl.edu.ph>
2025-11-08 22:34:40 +0800

    feat: remove username:

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     phone: { type: String },
