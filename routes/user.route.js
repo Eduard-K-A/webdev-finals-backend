660ab740e5bad743f190349fd82add033f9e2c09
Eduard <eduard_king_anterola@dlsl.edu.ph>
2025-11-14 17:12:26 +0800

    feat: add user route

---
+++ routes/user.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { getAllUsers, getUserById } from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+// GET /api/users - list all users
+router.get('/users', getAllUsers);
+
+// GET /api/users/:id - get single user
+router.get('/users/:id', getUserById);
+
+export default router;
