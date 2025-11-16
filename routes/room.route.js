e68f6323d569db632e25169ef6cc0c848aa21773
Eduard <eduard_king_anterola@dlsl.edu.ph>
2025-11-16 18:16:59 +0800

    feat: add route GET for rooms by id

--- routes/room.route.js
+++ routes/room.route.js
@@ -2,20 +2,20 @@
  * Room API routes
  */
 import express from 'express';
-import { createRoom, getRooms } from '../controllers/room.controller.js';
+import { createRoom, getRooms, getRoomById, updateRoom } from '../controllers/room.controller.js';
 
 const router = express.Router();
 
 // POST /api/rooms - create a room
 router.post('/rooms', createRoom);
 
-// GET /api/rooms - list rooms
+// GET /api/rooms - list all rooms
 router.get('/rooms', getRooms);
 
-// GET /api/rooms/:id - get room by ID
-router.get('/rooms/:id', getRooms);
+// GET /api/rooms/:id - get room by ID (uuid or _id)
+router.get('/rooms/:id', getRoomById);
 
-//PUT /api/rooms/:id - update room
-router.put('/rooms/:id', createRoom);
+// PUT /api/rooms/:id - update room by ID (uuid or _id)
+router.put('/rooms/:id', updateRoom);
 
 export default router;
