fcd691a3f3080557691af638f28c0e5a6f1fa099
Eduard <eduard_king_anterola@dlsl.edu.ph>
2025-11-19 15:34:46 +0800

    fix: fix booking validation date

--- models/Booking.js
+++ models/Booking.js
@@ -5,7 +5,16 @@ const bookingSchema = new mongoose.Schema(
         user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
         room: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Room" },
         checkInDate: { type: Date, required: true, min: Date.now },
-        checkOutDate: { type: Date, required: true, validate: (checkOutDate, checkInDate) => checkOutDate > checkInDate },
+        checkOutDate: {
+            type: Date,
+            required: true,
+            validate: {
+                validator: function (value) {
+                    return value > this.checkInDate;
+                },
+                message: "Check-out date must be after the check-in date.",
+            },
+        },
         totalGuests: { type: Number, required: true },
         totalPrice: { type: Number, required: true, min: 0 },
         status: { type: String, enum: ["Pending", "Confirmed", "Cancelled", "Completed"], default: "Pending" },
